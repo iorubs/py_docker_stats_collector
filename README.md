7cf5fbce7cadeb41ba77280ec7ee491d69f7b02c
pro1oh1 <pro1oh1@gmail.com>
2015-09-28 10:49:34 +0100

    first realease

--- README.md
+++ README.md
@@ -2,16 +2,25 @@
 
 project started on 21-09-2015
 
-First working version expected release date: 27-09-2015
+Data is stored in a mysql db, follow the instructions on this website to get it installed and set up. http://zetcode.com/db/mysqlpython/
 
 
-This tool will collect stats similarly to cadvisor, but unlike cadvisor all stats and graphs will be stored locally so they can be used by any other applications or if you just want to leave it running in the background collecting data and come back a later time to check the results.
+This tool collects stats similarly to cadvisor, stats are stored in a db of your choosing in this case docker_stats. Just run python collector.py and leave it running in the background collecting data and so you can come back a later time to check the results.
 
 
-Once the first version is developed I will update this readme file with the usage instructions
 
 
-MORE INFO: With this tool you will be able to store and analyse cpu usage, individual core usage and ram_usage for now, the data will be presented to the user in 2 different ways, a text version not sure about which format yet, and a graphic version(graphs will be stored in some location passed in by the user). More info coming soon...
+MORE INFO: With this tool you will be able to store and analyse cpu usage, individual core usage and ram_usage for now.
+
+To access the data open a terminal window and run mysql:  
+mysql -u root -p  
+mysql> USE docker_stats;  
+SELECT * FROM container_id;
+
+
+TODO and more INFO:  
+
+
 
 
 Developer: Ruben Vasconcelos ruben.vasconcelos2@mail.dcu.ie
--- collector.py
+++ collector.py
@@ -4,26 +4,17 @@ Container stats pulled from /sys/fs/cgroup,
 Graphs created using :
 """
 
-import argparse
 import subprocess
+import MySQLdb as mdb
 import os
 import time
+import sys
+
+#bash table_drop.sh collector GetMe2ome2t@t2 docker_stats
 
 
-ARGS = {'time_frame': 5, 'home_dir': ''}
+ARGS = {'time_frame': 0.01, 'mode': 'collect'}
 
-def read_args():
-    """Read in arguments passed in by user"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--time-frame', '-tf', nargs=1, dest='time_frame', type=float, required=False, help='Time interval between data polls')
-    parser.add_argument('--home-dir', '-hm', nargs=1 , dest='home_dir', type=str, required=True, help='Path to data save location')
-    args = parser.parse_args()
-    
-    if args.time_frame != None:
-        ARGS['time_frame'] = args.time_frame[0]
-
-    if args.home_dir != None:
-        ARGS['home_dir'] = args.home_dir[0]
 
 def get_docker_ids():
     """Get long docker Container id's"""
@@ -34,92 +25,102 @@ def get_docker_ids():
     for line in proc.stdout.readlines():
         id = line.split( )
         docker_ids.append(id[0])
-        storage_prep(id[0])
     retval = proc.wait()
 
     return docker_ids
 
-def storage_prep(container_id):
-    """Create container's data storage location and required storage files"""
-    dir = ARGS['home_dir'] + '/stats/' + container_id
-
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-        cpu = dir + '/cpu.txt'
-        open(cpu, 'a').close()
-        core = dir + '/core.txt'
-        open(core, 'a').close()
-        memory = dir + '/memory.txt'
-        open(memory, 'a').close()
-        ram = dir + '/ram.txt'
-        open(ram, 'a').close()
-
-def pull_data(command, storage_file):
-    """Run command and append results to storage_file"""
-
-    echo_command = 'echo $(' + command + ') >> ' + storage_file 
-
-    proc = subprocess.Popen(echo_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    _, _ = proc.communicate()
-    retval = proc.wait()
-
-
-def get_cpu_usage(docker_ids):
+def get_cpu_usage(docker_id):
     """Collect container cpu usage data for each running continer,
     /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage"""
 
-    location  = 'cat /sys/fs/cgroup/cpuacct/docker/'
+    location  = '/sys/fs/cgroup/cpuacct/docker/'
     data_file = '/cpuacct.usage'
+    command = location + docker_id + data_file
+    if os.path.exists(command):
+        command = 'cat ' + command
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out, _ = proc.communicate()
+        retval = proc.wait()
 
-    for id in docker_ids:
-        command = location + id + data_file
-        storage_file = ARGS['home_dir'] + '/stats/' + id + '/cpu.txt'
-        pull_data(command, storage_file)
+        data = out.split('\n')
+
+        return data[0]
+
+    return -1
 
 
-def get_core_usage(docker_ids):
+def get_core_usage(docker_id):
     """Collect individual core usage data for each running continer,
     /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage_percpu"""
 
-    location  = 'cat /sys/fs/cgroup/cpuacct/docker/'
+    location  = '/sys/fs/cgroup/cpuacct/docker/'
     data_file = '/cpuacct.usage_percpu'
 
-    for id in docker_ids:
-        command = location + id + data_file
-        storage_file = ARGS['home_dir'] + '/stats/' + id + '/core.txt'
-        pull_data(command, storage_file)
+    command = location + docker_id + data_file
 
-def get_memory_usage(docker_ids):
+    if os.path.exists(command):
+        command = 'cat ' + command
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out, _ = proc.communicate()
+        retval = proc.wait()
+
+        data = out.split('\n')
+        data = data[0].split(' ')
+
+        return (data[0], data[1], data[2], data[3])
+
+    return -1
+
+
+def get_memory_usage(docker_id):
     """Collect memory usage data for each running container,
     /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.usage_in_bytes"""
 
     location  = 'cat /sys/fs/cgroup/memory/docker/'
     data_file = '/memory.usage_in_bytes'
 
-    for id in docker_ids:
-        command = location + id + data_file
-        storage_file = ARGS['home_dir'] + '/stats/' + id + '/memory.txt'
-        pull_data(command, storage_file)
+    command = location + docker_id + data_file
+
+    if os.path.exists(command):
+        command = 'cat ' + command
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out, _ = proc.communicate()
+        retval = proc.wait()
+
+        data = out.split('\n')
+
+        return data[0]
+
+    return -1
 
 def get_ram_usage(docker_ids):
     """"""
 
+
 def main():
     """Main method call helper fuctions"""
-    read_args()
 
-    dir = ARGS['home_dir'] + '/stats'
+    con = mdb.connect('localhost', 'collector', 'GetMe2ome2t@t2', 'docker_stats');
 
-    if not os.path.exists(dir):
-        os.mkdir(dir)
+    with con:
+        cur = con.cursor()
+        try:
+            while True:
+                docker_ids = get_docker_ids()
+                for id in docker_ids:
+                    create = "CREATE TABLE IF NOT EXISTS " + str(id) + "(Id INT PRIMARY KEY AUTO_INCREMENT, Cpu INT, Core1 INT, Core2 INT, Core3 INT, Core4 INT, Memory INT, Ram INT)"
+                    cur.execute(create)
+                    cpu_data = get_cpu_usage(id)
+                    core_data = get_core_usage(id)
+                    memory_data = get_memory_usage(id)
+                    #ram_data = get_ram_usage(id)
 
-    while True:
-        docker_ids = get_docker_ids()
-        get_cpu_usage(docker_ids)
-        get_core_usage(docker_ids)
-        get_memory_usage(docker_ids)
-        get_ram_usage(docker_ids)
-        time.sleep(ARGS['time_frame'])
-
+                    if cpu_data != -1 and core_data != -1 and memory_data != -1:
+                        insert = "INSERT INTO " + str(id) + "(Cpu,Core1,Core2,Core3,Core4,Memory,Ram) VALUES(" + str(cpu_data) + "," + core_data[0] + "," + core_data[1] + "," + core_data[2] + "," + core_data[3] + "," + str(memory_data) + ",0)"
+                        cur.execute(insert)
+                time.sleep(ARGS['time_frame'])
+        except KeyboardInterrupt:
+            print "Exiting"
+            sys.exit()
 
 main()
