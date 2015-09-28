13b29a295b352c172aa83d60fb31fedcef422706
pro1oh1 <pro1oh1@gmail.com>
2015-09-28 17:01:52 +0100

    cleaner code

--- collector.py
+++ collector.py
@@ -21,10 +21,12 @@ def read_args():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', '-m', nargs=1, dest='mode', type=str, required=False, help='Mode type (collect or analyse)')
-    parser.add_argument('--container-id', '-id', nargs=1, dest='container_id', type=str, required=False, help='Enter container ID')
+    parser.add_argument('--mode', '-m', nargs=1, dest='mode', type=str,
+                        required=False, help='Mode type (collect or analyse)')
+    parser.add_argument('--container-id', '-id', nargs=1, dest='container_id',
+                        type=str, required=False, help='Enter container ID')
     args = parser.parse_args()
-    
+
     if args.mode != None:
         ARGS['mode'] = args.mode[0]
 
@@ -41,11 +43,12 @@ def get_docker_ids():
 
     docker_ids = []
 
-    proc = subprocess.Popen('docker ps -q --no-trunc', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen('docker ps -q --no-trunc', shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in proc.stdout.readlines():
-        id = line.split( )
-        docker_ids.append(id[0])
-    retval = proc.wait()
+        container_id = line.split( )
+        docker_ids.append(container_id[0])
+    proc.wait()
 
     return docker_ids
 
@@ -55,14 +58,15 @@ def get_cpu_usage(docker_id):
     /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage
     """
 
-    location  = '/sys/fs/cgroup/cpuacct/docker/'
+    location = '/sys/fs/cgroup/cpuacct/docker/'
     data_file = '/cpuacct.usage'
     command = location + docker_id + data_file
     if os.path.exists(command):
         command = 'cat ' + command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
         out, _ = proc.communicate()
-        retval = proc.wait()
+        proc.wait()
 
         data = out.split('\n')
 
@@ -77,16 +81,17 @@ def get_core_usage(docker_id):
     /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage_percpu
     """
 
-    location  = '/sys/fs/cgroup/cpuacct/docker/'
+    location = '/sys/fs/cgroup/cpuacct/docker/'
     data_file = '/cpuacct.usage_percpu'
 
     command = location + docker_id + data_file
 
     if os.path.exists(command):
         command = 'cat ' + command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
         out, _ = proc.communicate()
-        retval = proc.wait()
+        proc.wait()
 
         data = out.split('\n')
         data = data[0].split(' ')
@@ -102,16 +107,17 @@ def get_memory_usage(docker_id):
     /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.usage_in_bytes
     """
 
-    location  = '/sys/fs/cgroup/memory/docker/'
+    location = '/sys/fs/cgroup/memory/docker/'
     data_file = '/memory.usage_in_bytes'
 
     command = location + docker_id + data_file
 
     if os.path.exists(command):
         command = 'cat ' + command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
         out, _ = proc.communicate()
-        retval = proc.wait()
+        proc.wait()
 
         data = out.split('\n')
 
@@ -133,7 +139,8 @@ def empty_db():
 
     drop_tables = "sudo bash table_drop.sh " + ARGS['db_user'] + ' ' \
                   + ARGS['db_password'] + ' ' + ARGS['db_name']
-    proc = subprocess.Popen(drop_tables, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc = subprocess.Popen(drop_tables, shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
     out, _ = proc.communicate()
 
     print out
@@ -146,10 +153,11 @@ def collect_data():
     """
     Run in a loop collecting container stats
     """
-    
+
     empty_db()
 
-    con = mdb.connect(ARGS['db_location'], ARGS['db_user'], ARGS['db_password'], ARGS['db_name']);
+    con = mdb.connect(ARGS['db_location'], ARGS['db_user'],
+                      ARGS['db_password'], ARGS['db_name'])
 
     with con:
         cur = con.cursor()
@@ -157,16 +165,22 @@ def collect_data():
         try:
             while True:
                 docker_ids = get_docker_ids()
-                for id in docker_ids:
-                    create = "CREATE TABLE IF NOT EXISTS " + str(id) + "(Id INT PRIMARY KEY AUTO_INCREMENT, Cpu INT, Core1 INT, Core2 INT, Core3 INT, Core4 INT, Memory INT, Ram INT)"
+                for container_id in docker_ids:
+                    create = "CREATE TABLE IF NOT EXISTS " + str(container_id) \
+                    + "(Id INT PRIMARY KEY AUTO_INCREMENT, Cpu INT, Core1 INT" \
+                    + ", Core2 INT, Core3 INT, Core4 INT, Memory INT, Ram INT)"
                     cur.execute(create)
-                    cpu_data = get_cpu_usage(id)
-                    core_data = get_core_usage(id)
-                    memory_data = get_memory_usage(id)
-                    ram_data = get_ram_usage(id)
+                    cpu_data = get_cpu_usage(container_id)
+                    core_data = get_core_usage(container_id)
+                    memory_data = get_memory_usage(container_id)
+                    ram_data = get_ram_usage(container_id)
 
                     if cpu_data != -1 and core_data != -1 and memory_data != -1 and ram_data != -1:
-                        insert = "INSERT INTO " + str(id) + "(Cpu,Core1,Core2,Core3,Core4,Memory,Ram) VALUES(" + str(cpu_data) + "," + core_data[0] + "," + core_data[1] + "," + core_data[2] + "," + core_data[3] + "," + str(memory_data) + ",00000)"
+                        insert = "INSERT INTO " + str(container_id) \
+                        + "(Cpu,Core1,Core2,Core3,Core4,Memory,Ram) VALUES(" \
+                        + str(cpu_data) + "," + core_data[0] + "," \
+                        + core_data[1] + "," + core_data[2] + "," \
+                        + core_data[3] + "," + str(memory_data) + ",00000)"
                         cur.execute(insert)
                 time.sleep(ARGS['time_frame'])
         except KeyboardInterrupt:
@@ -178,9 +192,11 @@ def analyse_data():
     """
     Retrieve container's stats
     """
-    con = mdb.connect(ARGS['db_location'], ARGS['db_user'], ARGS['db_password'], ARGS['db_name']);
 
-    with con: 
+    con = mdb.connect(ARGS['db_location'], ARGS['db_user'],
+                      ARGS['db_password'], ARGS['db_name'])
+
+    with con:
         cur = con.cursor()
         cur.execute("SELECT * FROM " + ARGS['container_id'])
 
@@ -188,7 +204,7 @@ def analyse_data():
 
         print "(T - Cpu - Core1 - Core2 - Core3 - Core4 - Memory - Ram)"
         for row in rows:
-            print row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7] 
+            print row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
 
 
 def main():
