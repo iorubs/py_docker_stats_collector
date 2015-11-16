921a62470200b99cf34c7be56bcf84203b7aea79
pro1oh1 <pro1oh1@gmail.com>
2015-11-16 17:53:13 +0000

    Version 1.0

--- README.md
+++ README.md
@@ -1,36 +1,32 @@
 # py_docker_stats_collector
 
-project started on 21-09-2015
+project started on 21-09-2015  
 
-Data is stored in a mysql db, follow the instructions on this website to get it installed and set up. http://zetcode.com/db/mysqlpython/
+This tool collects stats similarly to cadvisor, but in addition it also stores them. So you can use it with other programs or if you simply want to keep a record of your containers stats.
 
-
-This tool collects stats similarly to cadvisor, stats are stored in a db of your choosing in this case docker_stats. Just run python collector.py and leave it running in the background collecting data and so you can come back a later time to check the results.
-
-
-
+To change the way data is stored/displayed, update save_data() or analyse_data() respectively.  
 
 MORE INFO: With this tool you will be able to store and analyse cpu usage, individual core usage and memory usage for now.
+
+Usage help: python collector.py --help  
 
 Collector mode: python collector.py  
 
 Analyser mode: python collector.py -m 'analyse' -id $(container_id)  
 
 Example output:  
-(T - Cpu - Core1 - Core2 - Core3 - Core4 - Memory)  
-1 15868738 4168148 8452622 1585063 1662905 544768  
-2 ...   
+cpu core1 core2 core3 core4 memory  
+15868738 4168148 8452622 1585063 1662905 544768  
+...   
 
 
-T/100 = time since container startup in secons. Ex = if T=1 than time in secs = 0.01  
+By default the time frame for collection is 0.01 so the line number/100 = time since container startup in secons. Ex = num of lines=1 than runtime in secs = 0.01  
 
 All other values are in bytes.  
 
 TODO and more INFO:  
-Testing, fix bugs + add networking stats.  
-
-
+Testing, fix bugs + add io stats.  
 
 
 Developer: Ruben Vasconcelos ruben.vasconcelos2@mail.dcu.ie
-Feel free to email me with any sugestions/feature requests
+Feel free to email me with any sugestions/feature or requests.
--- collector.py
+++ collector.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
 """
-This tool gathers and saves Container stats pulled from /sys/fs/cgroup,
-in a mysql db.
+Container stats pulled from /sys/fs/cgroup,
+Graphs created using :
 """
 
 import argparse
 import subprocess
-import MySQLdb as mdb
 import os
 import time
 import sys
 
-ARGS = {'time_frame': 0.01, 'mode': 'collect', 'db_user': 'collector',
-        'db_password': 'GetMe2ome2t@t2', 'db_location': 'localhost',
-        'db_name': 'docker_stats', 'container_id': ''}
+
+ARGS = {'time_frame': 0.1, 'home_dir': '',
+    'mode': 'collect', 'container_id': ''}
 
 def read_args():
-    """
-    Read in arguments passed in by user
-    """
-
+    """Read in arguments passed in by user"""
     parser = argparse.ArgumentParser()
+    parser.add_argument('--time-frame', '-tf', nargs=1, dest='time_frame',
+                        type=float, required=False,
+                        help='Time interval between data colection.')
     parser.add_argument('--mode', '-m', nargs=1, dest='mode', type=str,
                         required=False, help='Mode type (collect or analyse)')
     parser.add_argument('--container-id', '-id', nargs=1, dest='container_id',
-                        type=str, required=False, help='Enter container ID')
+                        type=str, required=False, help='Enter container ID.')
     args = parser.parse_args()
+
+    if args.time_frame != None:
+        ARGS['time_frame'] = args.time_frame
 
     if args.mode != None:
         ARGS['mode'] = args.mode[0]
@@ -35,178 +37,136 @@ def read_args():
         elif ARGS['mode'] == 'analyse':
             print 'You have to supply a container ID.'
 
+    ARGS['home_dir'] = os.getcwd() + '/stats'
+
+    if not os.path.exists(ARGS['home_dir']):
+        os.mkdir(ARGS['home_dir'])
+
+
 
 def get_docker_ids():
-    """
-    Get long docker Container id's
-    """
+    """Get long docker Container id's"""
 
     docker_ids = []
 
     proc = subprocess.Popen('docker ps -q --no-trunc', shell=True,
-                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+           stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in proc.stdout.readlines():
-        container_id = line.split( )
-        docker_ids.append(container_id[0])
+        docker_id = line.split()
+        docker_ids.append(docker_id[0])
+        storage_prep(docker_id[0])
     proc.wait()
 
     return docker_ids
 
-def get_cpu_usage(docker_id):
-    """
-    Collect container cpu usage data for each running continer,
-    /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage
-    """
+def storage_prep(container_id):
+    """Create container's data storage location """
+    storage_location = ARGS['home_dir'] + '/' + container_id
 
-    location = '/sys/fs/cgroup/cpuacct/docker/'
-    data_file = '/cpuacct.usage'
-    command = location + docker_id + data_file
+    if not os.path.exists(storage_location):
+        with open(storage_location, "a") as stats_file:
+            stats_file.write("cpu core1 core2 core3 core4 memory")
+            stats_file.write("\n")
+
+
+def get_data(command):
+    """Cat stats file and return data"""
+    data = ['']
+
     if os.path.exists(command):
         command = 'cat ' + command
         proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
+               stderr=subprocess.STDOUT)
         out, _ = proc.communicate()
+        data = out.split('\n')
         proc.wait()
 
-        data = out.split('\n')
+    return data[0]
 
-        return data[0]
 
-    return -1
+def get_cpu_usage(docker_id):
+    """Collect container cpu usage data for each running continer,
+    /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage"""
 
+    location = '/sys/fs/cgroup/cpuacct/docker/'
+    data_file = '/cpuacct.usage'
+
+    command = location + docker_id + data_file
+    return get_data(command)
 
 def get_core_usage(docker_id):
-    """
-    Collect individual core usage data for each running continer,
-    /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage_percpu
-    """
+    """Collect individual core usage data for each running continer,
+    /sys/fs/cgroup/cpuacct/docker/$CONTAINER_ID/cpuacct.usage_percpu"""
 
     location = '/sys/fs/cgroup/cpuacct/docker/'
     data_file = '/cpuacct.usage_percpu'
 
     command = location + docker_id + data_file
-
-    if os.path.exists(command):
-        command = 'cat ' + command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-        out, _ = proc.communicate()
-        proc.wait()
-
-        data = out.split('\n')
-        data = data[0].split(' ')
-
-        return (data[0], data[1], data[2], data[3])
-
-    return -1
-
+    return get_data(command)
 
 def get_memory_usage(docker_id):
-    """
-    Collect memory usage data for each running container,
-    /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.usage_in_bytes
-    """
+    """Collect memory usage data for each running container,
+    /sys/fs/cgroup/memory/docker/$CONTAINER_ID/memory.usage_in_bytes"""
 
     location = '/sys/fs/cgroup/memory/docker/'
     data_file = '/memory.usage_in_bytes'
 
     command = location + docker_id + data_file
+    return get_data(command)
 
-    if os.path.exists(command):
-        command = 'cat ' + command
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-        out, _ = proc.communicate()
-        proc.wait()
 
-        data = out.split('\n')
-
-        return data[0]
-
-    return -1
-
-def empty_db():
+def save_data(cpu, core, memory, docker_id):
     """
-    Drop all existing tables in the db,
-    this runs once before collecting new data.
+    Save stats in the correct locations, and correct format.
     """
 
-    drop_tables = "sudo bash table_drop.sh " + ARGS['db_user'] + ' ' \
-                  + ARGS['db_password'] + ' ' + ARGS['db_name']
-    proc = subprocess.Popen(drop_tables, shell=True, stdout=subprocess.PIPE,
-                            stderr=subprocess.STDOUT)
-    out, _ = proc.communicate()
+    cores = core.split()
+    entry = cpu + ' ' + cores[0] + ' ' + cores[1] + ' ' \
+            + cores[2] + ' ' + cores[3] + ' ' + memory
 
-    print out
-
-    retval = proc.wait()
-
-    return retval
+    storage_location = ARGS['home_dir'] + '/' + docker_id
+    with open(storage_location, "a") as stats_file:
+        stats_file.write(str(entry))
+        stats_file.write("\n")
 
 def collect_data():
     """
     Run in a loop collecting container stats
     """
-
-    empty_db()
-
-    con = mdb.connect(ARGS['db_location'], ARGS['db_user'],
-                      ARGS['db_password'], ARGS['db_name'])
-
-    with con:
-        cur = con.cursor()
-        print 'Running ...'
-        try:
-            while True:
-                docker_ids = get_docker_ids()
-                for container_id in docker_ids:
-                    create = "CREATE TABLE IF NOT EXISTS " + str(container_id) \
-                    + "(Id INT PRIMARY KEY AUTO_INCREMENT, Cpu INT, Core1 INT" \
-                    + ", Core2 INT, Core3 INT, Core4 INT, Memory INT)"
-                    cur.execute(create)
-                    cpu_data = get_cpu_usage(container_id)
-                    core_data = get_core_usage(container_id)
-                    memory_data = get_memory_usage(container_id)
-
-                    if cpu_data != -1 and core_data != -1 and memory_data != -1:
-                        insert = "INSERT INTO " + str(container_id) \
-                        + "(Cpu,Core1,Core2,Core3,Core4,Memory) VALUES(" \
-                        + str(cpu_data) + "," + core_data[0] + "," \
-                        + core_data[1] + "," + core_data[2] + "," \
-                        + core_data[3] + "," + str(memory_data) + ")"
-                        cur.execute(insert)
+    try:
+        while True:
+            docker_ids = get_docker_ids()
+            for docker_id in docker_ids:
+                cpu = get_cpu_usage(docker_id)
+                core = get_core_usage(docker_id)
+                memory = get_memory_usage(docker_id)
+                save_data(cpu, core, memory, docker_id)
                 time.sleep(ARGS['time_frame'])
-        except KeyboardInterrupt:
-            print "Exiting"
-            sys.exit()
-
+    except KeyboardInterrupt:
+        print ""
+        print "Received user exit request!"
+        sys.exit()
 
 def analyse_data():
     """
     Retrieve container's stats
     """
 
-    con = mdb.connect(ARGS['db_location'], ARGS['db_user'],
-                      ARGS['db_password'], ARGS['db_name'])
+    file_location = ARGS['home_dir'] + '/' + ARGS['container_id']
+    content = []
 
-    with con:
-        cur = con.cursor()
-        cur.execute("SELECT * FROM " + ARGS['container_id'])
+    with open(file_location, 'r') as stats_file:
+        for line in stats_file:
+            line = line.rstrip('\n')
+            line = line.split()
+            entry = [line[0], line[1], line[2], line[3], line[4], line[5]]
+            content.append(entry)
 
-        rows = cur.fetchall()
-
-        print "(T - Cpu - Core1 - Core2 - Core3 - Core4 - Memory)"
-        for row in rows:
-            print row[0], row[1], row[2], row[3], row[4], row[5], row[6]
-
+    print content
 
 def main():
-    """
-    Main method call helper fuctions
-    """
-
+    """Main method call helper fuctions"""
     read_args()
-
 
     if ARGS['mode'] == 'collect':
         collect_data()
@@ -214,6 +174,5 @@ def main():
         analyse_data()
     else:
         print ARGS['mode'] + ' is not a valid mode.'
-
 
 main()
